a094414a5ee62d82f402fea856c92e7774e051c7
Denis Shubin <denis.shubin@gmail.com>
2022-07-25 14:27:46 +0300

    refactor: error404

--- src/common/error-page/Error404.tsx
+++ src/common/error-page/Error404.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import error404 from '../../assets/img/404.gif'
+import styles from './Error404.module.css'
 
 export const Error404 = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <img src={error404} alt={'Page not found'}/>
         </div>
     );
