3d65eaebb35a90861f558414fdc1c53e11dfbf07
Denis Shubin <denis.shubin@gmail.com>
2022-07-25 14:30:51 +0300

    feat: add Error progressbar and Status progressbar

--- src/bll/reducers/app-reducer.ts
+++ src/bll/reducers/app-reducer.ts
@@ -1,14 +1,26 @@
-const initialState = {}
+const initialState = {
+    status: 'idle' as RequestStatusType,
+    error: null as null | string
+}
 
 type InitialStateType = typeof initialState
 
-export type ActionsType = ReturnType<typeof anyAC>
-
 export const appReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
+        case 'SET-ERROR':
+            return {...state, error: action.error}
+        case 'SET-STATUS':
+            return {...state, status: action.status}
         default:
             return state
     }
 }
 
-export const anyAC = () => ({type: ''} as const)
+// actions
+export const setAppErrorAC = (error: null | string) => ({type: 'SET-ERROR', error} as const)
+export const setAppStatusAC = (status: RequestStatusType) => ({type: 'SET-STATUS', status} as const)
+
+// types
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+type ActionsType = ReturnType<typeof setAppErrorAC> | ReturnType<typeof setAppStatusAC>
---
+++ src/components/ErrorSnackbar/ErrorSnackbar.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, {AlertProps} from '@mui/material/Alert';
+import {useAppDispatch, useAppSelector} from '../../bll/store';
+import {setAppErrorAC} from '../../bll/reducers/app-reducer';
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+    props,
+    ref,
+) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+export const ErrorSnackbar = () => {
+    const error = useAppSelector(state => state.app.error)
+
+    const dispatch = useAppDispatch()
+
+    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        dispatch(setAppErrorAC(null))
+    };
+
+    return (
+        <Snackbar
+            open={error !== null}
+            autoHideDuration={6000}
+            onClose={handleClose}
+            anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
+        >
+            <Alert onClose={handleClose} severity="error" sx={{width: '100%'}}>
+                {error}
+            </Alert>
+        </Snackbar>
+
+    );
+}
