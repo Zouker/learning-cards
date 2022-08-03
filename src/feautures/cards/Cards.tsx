725a9dff7c569f360af2882f8491b9809346db0b
Denis Shubin <denis.shubin@gmail.com>
2022-08-03 22:42:00 +0300

    feat: pagination for cards

--- src/feautures/cards/Cards.tsx
+++ src/feautures/cards/Cards.tsx
@@ -1,34 +1,65 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useAppDispatch, useAppSelector} from '../../bll/store';
-import {addCardTC, getCardsTC} from '../../bll/reducers/cards-reducer';
-import {useParams} from 'react-router-dom';
+import {addCardTC, getCardsTC, searchQuestionAC, setPackIdAC, setPackNameAC} from '../../bll/reducers/cards-reducer';
+import {useNavigate, useParams} from 'react-router-dom';
 import {CardsTable} from './CardsTable';
 import {Button} from '@mui/material';
+import styles from './Cards.module.css'
 
 export const Cards = () => {
     const dispatch = useAppDispatch()
-    const {cardsPack_id} = useParams()
-    const packName = useAppSelector(state => state.packs.cardPacks.find(pack => pack._id === cardsPack_id)?.name)
+    const {cardsPack} = useParams()
+    const page = useAppSelector(state => state.cards.params.page)
+    const pageCount = useAppSelector(state => state.cards.params.pageCount)
+    const packs = useAppSelector(state => state.packs.cardPacks)
+    const packName = useAppSelector(state => state.cards.packName)
+    const userId = useAppSelector(state => state.profile._id)
+    const packUserId = useAppSelector(state => state.cards.packUserId)
+    const question = useAppSelector(state => state.cards.params.cardQuestion)
+    const [name, id] = cardsPack?.split('~') as string[]
+    const navigate = useNavigate()
 
-    console.log(cardsPack_id)
+    const [value, setValue] = useState('')
+
     const addCard = () => {
         const question = 'HARDCODE QUESTION'
         const answer = 'HARDCODE ANSWER'
-        if (cardsPack_id) {
-            dispatch(addCardTC(cardsPack_id, question, answer))
+        if (id) {
+            dispatch(addCardTC(id, question, answer))
         }
     }
 
+    const onClickBackHandler = () => {
+        navigate(-1)
+    }
+
     useEffect(() => {
-        if (cardsPack_id) {
-            dispatch(getCardsTC({cardsPack_id}))
+        if (id) {
+            dispatch(setPackIdAC(id))
+            dispatch(setPackNameAC(name))
+            dispatch(getCardsTC())
         }
-    }, [dispatch, cardsPack_id])
+    }, [dispatch, id, name, page, pageCount, packUserId, packs, question])
 
     return (
         <div>
-            {packName}
-            <Button variant={'contained'} onClick={addCard}>Add new card</Button>
+            <div onClick={onClickBackHandler} className={styles.backButton}>
+                <svg width="16" height="11" viewBox="0 0 16 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M16 5.5H2M2 5.5L6.66667 1M2 5.5L6.66667 10" stroke="black" strokeWidth="2"/>
+                </svg>
+                <div className={styles.back}>Back to Packs List</div>
+            </div>
+            <div className={styles.packName}>{packName}</div>
+            {userId === packUserId
+                ? <div className={styles.addCardButton}>
+                    <Button variant={'contained'} onClick={addCard}>Add new card</Button>
+                </div>
+                : <></>}
+            <input type={'search'} value={value}
+                   onChange={(e) => {
+                       setValue(e.currentTarget.value)
+                       dispatch(searchQuestionAC(e.currentTarget.value))
+                   }}/>
             <CardsTable/>
         </div>
     );
