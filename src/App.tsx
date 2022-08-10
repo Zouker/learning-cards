2cf492e6694eb5911eb47339a3df1a0c81f4fe0d
YuryHouse <yury.house@yahoo.com>
2022-08-10 15:57:49 +0300

    App.tsx was fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {RoutesPage} from './routes/RoutesPage';
 import {ErrorSnackbar} from './components/ErrorSnackbar/ErrorSnackbar';
-import {Button, CircularProgress, LinearProgress} from '@mui/material';
+import {CircularProgress, LinearProgress} from '@mui/material';
 import {useAppDispatch, useAppSelector} from './bll/store';
 import {initializeAppTC} from './feautures/auth/login/login-reducer';
-import {DeletePackModal} from "./feautures/modals/modals-packs/DeletePackModal";
 
 
 function App() {
-    const[isDeletePackModalOpen, setIsDeletePackModalOpen] = useState<boolean>(false)
     const dispatch = useAppDispatch()
     const isInitialized = useAppSelector(state => state.login.isInitialized)
     const status = useAppSelector(state => state.app.status)
@@ -28,11 +26,6 @@ function App() {
     }
 
 
-    const deleteModalHandler = () => {
-        setIsDeletePackModalOpen(!isDeletePackModalOpen)
-    }
-
-
     return (
         <div className="App">
             <div>
@@ -40,12 +33,6 @@ function App() {
                 <ErrorSnackbar/>
                 <RoutesPage/>
             </div>
-            <div className="controls">
-                <Button variant="contained" onClick={deleteModalHandler}>Open first modal</Button>
-                <Button variant="contained">Open second modal</Button>
-                <Button variant="contained">Open third modal</Button>
-            </div>
-           <DeletePackModal isModalOpen={isDeletePackModalOpen} setIsModalOpen={deleteModalHandler}/>
         </div>
 
     );
