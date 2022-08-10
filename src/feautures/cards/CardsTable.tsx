af8da03560238b2ff5a863015d548573f11d0af4
YuryHouse <yury.house@yahoo.com>
2022-08-10 21:44:04 +0300

    DeleteCardModal.tsx was to CardsTable.tsx added

--- src/feautures/cards/CardsTable.tsx
+++ src/feautures/cards/CardsTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -12,8 +12,10 @@ import {useAppDispatch, useAppSelector} from '../../bll/store';
 import {Rating, TableHead} from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CreateIcon from '@mui/icons-material/Create';
-import {deleteCardTC, setCardPageAC, setCardPageCountAC, updateCardTC} from '../../bll/reducers/cards-reducer';
+import {setCardPageAC, setCardPageCountAC, updateCardTC} from '../../bll/reducers/cards-reducer';
 import {formatDate} from '../../common/format-date/formatDate';
+import {CardsType} from "./cardsAPI";
+import {DeleteCardModal} from "../modals/modals-cards/DeleteCardModal";
 
 export const CardsTable = () => {
     const dispatch = useAppDispatch()
@@ -22,6 +24,13 @@ export const CardsTable = () => {
     const pageCount = useAppSelector(state => state.cards.params.pageCount)
     const cardsTotalCount = useAppSelector(state => state.cards.cardsTotalCount)
     const userId = useAppSelector(state => state.profile._id)
+
+    const [deleteCardData, setDeleteCardData] = useState<CardsType | null>(null);
+    const [isOpenCardDeleteModal, setIsOpenCardDeleteModal] = useState(false);
+
+    // const [updateCardData, setUpdateCardData] = useState<CardsType | null>(null);
+    // const [isOpenCardUpdateModal, setIsOpenCardUpdateModal] = useState(false);
+
 
     const handleChangePage = (
         event: React.MouseEvent<HTMLButtonElement> | null,
@@ -37,8 +46,9 @@ export const CardsTable = () => {
         dispatch(setCardPageAC(1))
     };
 
-    const deleteCard = (packId: string, cardId: string) => {
-        dispatch(deleteCardTC(packId, cardId))
+    const openModalDeleteCard = (card: CardsType) => {
+        setIsOpenCardDeleteModal(true);
+        setDeleteCardData(card);
     }
 
     const updateCard = (packId: string, cardId: string) => {
@@ -48,60 +58,70 @@ export const CardsTable = () => {
     }
 
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{minWidth: 500}} aria-label="custom pagination table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell align="center">Question</TableCell>
-                        <TableCell align="center">Answer</TableCell>
-                        <TableCell align="center">Last Updated</TableCell>
-                        <TableCell align="center">Grade</TableCell>
-                        <TableCell align="center">Actions</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {cards.length ? cards.map((card) => (
-                            <TableRow key={card._id}>
-                                <TableCell align="center" component="th" scope="row">
-                                    {card.question}
-                                </TableCell>
-                                <TableCell align="center">
-                                    {card.answer}
-                                </TableCell>
-                                <TableCell align="center">
-                                    {formatDate(card.updated)}
-                                </TableCell>
-                                <TableCell align="center">
-                                    <Rating name="read-only" value={card.grade} readOnly/>
-                                </TableCell>
-                                <TableCell align="center">
-                                    <IconButton disabled={userId !== card.user_id}
-                                                onClick={() => updateCard(card.cardsPack_id, card._id)}>
-                                        <CreateIcon/>
-                                    </IconButton>
-                                    <IconButton disabled={userId !== card.user_id}
-                                                onClick={() => deleteCard(card.cardsPack_id, card._id)}>
-                                        <DeleteIcon/>
-                                    </IconButton>
-                                </TableCell>
-                            </TableRow>
-                        ))
-                        : <TableCell>CARDS NOT FOUND</TableCell>}
-                </TableBody>
-                <TableFooter>
-                    <TableRow>
-                        <TablePagination
-                            rowsPerPageOptions={[5, 10, 25]}
-                            colSpan={3}
-                            count={cardsTotalCount}
-                            rowsPerPage={pageCount}
-                            page={page - 1}
-                            onPageChange={handleChangePage}
-                            onRowsPerPageChange={handleChangeRowsPerPage}
-                        />
-                    </TableRow>
-                </TableFooter>
-            </Table>
-        </TableContainer>
+        <>
+            <TableContainer component={Paper}>
+                <Table sx={{minWidth: 500}} aria-label="custom pagination table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="center">Question</TableCell>
+                            <TableCell align="center">Answer</TableCell>
+                            <TableCell align="center">Last Updated</TableCell>
+                            <TableCell align="center">Grade</TableCell>
+                            <TableCell align="center">Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {cards.length ? cards.map((card) => (
+                                <TableRow key={card._id}>
+                                    <TableCell align="center" component="th" scope="row">
+                                        {card.question}
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        {card.answer}
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        {formatDate(card.updated)}
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        <Rating name="read-only" value={card.grade} readOnly/>
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        <IconButton disabled={userId !== card.user_id}
+                                                    onClick={() => updateCard(card.cardsPack_id, card._id)}>
+                                            <CreateIcon/>
+                                        </IconButton>
+                                        <IconButton disabled={userId !== card.user_id}
+                                                    onClick={() => openModalDeleteCard(card)}
+                                        >
+                                            <DeleteIcon/>
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                            : <TableCell>CARDS NOT FOUND</TableCell>}
+                    </TableBody>
+                    <TableFooter>
+                        <TableRow>
+                            <TablePagination
+                                rowsPerPageOptions={[5, 10, 25]}
+                                colSpan={3}
+                                count={cardsTotalCount}
+                                rowsPerPage={pageCount}
+                                page={page - 1}
+                                onPageChange={handleChangePage}
+                                onRowsPerPageChange={handleChangeRowsPerPage}
+                            />
+                        </TableRow>
+                    </TableFooter>
+                </Table>
+            </TableContainer>
+            {deleteCardData && <DeleteCardModal
+                isModalOpen={isOpenCardDeleteModal}
+                setIsModalOpen={setIsOpenCardDeleteModal}
+                _id={deleteCardData._id}
+                cardsPack_id={deleteCardData.cardsPack_id}
+            />
+            }
+        </>
     );
 }
--- src/feautures/modals/modals-cards/DeleteCardModal.tsx
+++ src/feautures/modals/modals-cards/DeleteCardModal.tsx
@@ -1,25 +1,29 @@
 import React, {FC, memo} from 'react';
 import {CommonModal} from "../CommonModal";
+import {deleteCardTC} from "../../../bll/reducers/cards-reducer";
+import {useAppDispatch} from "../../../bll/store";
 
 type DeleteCardModalPropsType = {
     isModalOpen: boolean
     setIsModalOpen: (value: boolean) => void
     cardName?: string
-    _id?: string
+    _id: string
+    cardsPack_id: string
 }
 
 export const DeleteCardModal: FC<DeleteCardModalPropsType> = memo(({
                                                                        isModalOpen,
                                                                        setIsModalOpen,
                                                                        cardName,
-                                                                       _id
+                                                                       _id,
+                                                                       cardsPack_id
                                                                    }) => {
 
-    // const dispatch = useAppDispatch();
+    const dispatch = useAppDispatch();
 
     const deleteCard = () => {
-        // dispatch(deleteCardTC(_id));
-        // setIsModalOpen(false)
+        dispatch(deleteCardTC(cardsPack_id, _id));
+        setIsModalOpen(false)
     }
 
     return (
