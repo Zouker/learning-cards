50e65ccd91e92a890b739ac350d4f945d0c2a65a
Denis Shubin <denis.shubin@gmail.com>
2022-08-20 16:40:06 +0300

    feat: add ability to add and update cover of the pack

--- src/feautures/modals/modals-packs/UpdatePackModal.tsx
+++ src/feautures/modals/modals-packs/UpdatePackModal.tsx
@@ -1,9 +1,14 @@
 import React, {FC, memo, useEffect, useState} from 'react';
-import {CommonModal} from "../CommonModal";
-import {TextField} from "@mui/material";
-import {CardPacksType} from "../../packs/packsAPI";
-import {useAppDispatch} from "../../../bll/store";
-import {updatePackTC} from "../../../bll/reducers/packs-reducer";
+import {CommonModal} from '../CommonModal';
+import {IconButton, TextField} from '@mui/material';
+import {CardPacksType} from '../../packs/packsAPI';
+import {useAppDispatch} from '../../../bll/store';
+import {updatePackTC} from '../../../bll/reducers/packs-reducer';
+import styles from './PackModal.module.css';
+import noImage from '../../../assets/img/no-image.svg';
+import {InputTypeFile} from '../../../components/InputTypeFile/InputTypeFile';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import {setAppErrorAC} from '../../../bll/reducers/app-reducer';
 
 type UpdatePackModalPropsType = {
     isModalOpen: boolean
@@ -16,18 +21,26 @@ export const UpdatePackModal: FC<UpdatePackModalPropsType> = memo(({
                                                                        setIsModalOpen,
                                                                        cardsPack
                                                                    }) => {
-    const [newPackName, setNewPackName] = useState<string>(cardsPack? cardsPack.name : '');
+    const [newPackName, setNewPackName] = useState<string>(cardsPack ? cardsPack.name : '');
+    const [deckCover, setDeckCover] = useState(cardsPack ? cardsPack.deckCover : noImage)
+    const [isImageBroken, setIsImageBroken] = useState(false)
 
     const dispatch = useAppDispatch();
 
     useEffect(() => {
         cardsPack && setNewPackName(cardsPack.name)
+        cardsPack && setDeckCover(cardsPack.deckCover)
     }, [cardsPack])
 
     const updateCardPack = () => {
-        cardsPack && dispatch(updatePackTC(cardsPack._id, newPackName, cardsPack.deckCover));
+        cardsPack && dispatch(updatePackTC(cardsPack._id, newPackName, deckCover));
         setNewPackName(newPackName)
         setIsModalOpen(false)
+    }
+
+    const errorHandler = () => {
+        setIsImageBroken(true)
+        dispatch(setAppErrorAC('Wrong image'))
     }
 
     return (
@@ -45,6 +58,30 @@ export const UpdatePackModal: FC<UpdatePackModalPropsType> = memo(({
                        value={newPackName}
                        onChange={(e) => setNewPackName(e.currentTarget.value)}
             />
+            Pack cover preview
+            <div className={styles.frame}>
+                {cardsPack?.deckCover
+                    ? <img
+                        src={isImageBroken ? deckCover : deckCover}
+                        className={styles.image}
+                        onError={errorHandler}
+                        alt="img"
+                    />
+                    : <img
+                        src={deckCover ? deckCover : noImage}
+                        className={styles.image}
+                        onError={errorHandler}
+                        alt="img"
+                    />
+                }
+            </div>
+            <InputTypeFile uploadImage={(image: string) => setDeckCover(image)}>
+                <div className={styles.uploadButton}>
+                    <IconButton component="span">
+                        <CloudUploadIcon color={'primary'}/>
+                    </IconButton>
+                </div>
+            </InputTypeFile>
             <div><p>Do you really want to change <b>{cardsPack!.name}</b>?</p></div>
             {/*! is Non-null Assertion Operator (Postfix !)*/}
         </CommonModal>
