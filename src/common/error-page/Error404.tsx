8d770d20fbce1984b55ef22712ce12bba7a8895c
Denis Shubin <denis.shubin@gmail.com>
2022-08-25 21:41:34 +0300

    feat: add button to return to homepage

--- src/common/error-page/Error404.tsx
+++ src/common/error-page/Error404.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import error404 from '../../assets/img/404.gif'
 import styles from './Error404.module.css'
+import {Button} from '@mui/material';
+import {useNavigate} from 'react-router-dom';
 
 export const Error404 = () => {
+    const navigate = useNavigate()
+    const onClickHandler = () => {
+        navigate('/login')
+    }
+
     return (
         <div className={styles.container}>
-            <img src={error404} alt={'Page not found'}/>
+            <div><img src={error404} alt={'Page not found'}/></div>
+            <Button variant={'contained'} color={'primary'} onClick={onClickHandler}>Back to home page</Button>
         </div>
     );
 };
