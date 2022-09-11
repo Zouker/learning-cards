42f5229944d1910a28d9bef75ddd814df96e3f95
Denis Shubin <denis.shubin@gmail.com>
2022-09-11 16:00:46 +0300

    feat: add user's name and avatar in the right corner of the navbar

--- src/feautures/navbar/Navbar.tsx
+++ src/feautures/navbar/Navbar.tsx
@@ -16,11 +16,14 @@ import {useAppDispatch, useAppSelector} from '../../redux/store';
 import profile from '../../assets/img/profile.svg'
 import logout from '../../assets/img/logout.svg'
 import styles from './Navbar.module.css'
+import defaultUser from '../../assets/img/user.svg'
 
 export const Navbar = () => {
     const navigate = useNavigate()
     const dispatch = useAppDispatch()
     const isLoggedIn = useAppSelector(state => state.login.isLoggedIn)
+    const userName = useAppSelector(state => state.profile.name)
+    const userAvatar = useAppSelector(state => state.profile.avatar)
     const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
     const location = useLocation()
 
@@ -44,15 +47,16 @@ export const Navbar = () => {
         <AppBar position="static" className={styles.wrapper}>
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
-                    <StyleIcon sx={{display: {xs: 'none', md: 'flex'}, mr: 1}}/>
+                    <StyleIcon sx={{display: {xs: 'flex', md: 'flex'}, mr: 1}}/>
                     <Typography
                         variant="h6"
                         noWrap
                         component="a"
                         sx={{
                             mr: 2,
-                            display: {xs: 'none', md: 'flex'},
+                            display: {xs: 'flex', md: 'flex'},
                             fontFamily: 'monospace',
+                            flexGrow: 1,
                             fontWeight: 700,
                             letterSpacing: '.3rem',
                             color: 'inherit',
@@ -61,72 +65,61 @@ export const Navbar = () => {
                     >
                         LEARNING CARDS
                     </Typography>
-                    <Typography
-                        variant="h5"
-                        noWrap
-                        component="a"
-                        href=""
-                        sx={{
-                            mr: 2,
-                            display: {xs: 'flex', md: 'none'},
-                            flexGrow: 1,
-                            fontFamily: 'monospace',
-                            fontWeight: 700,
-                            letterSpacing: '.3rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                        }}
-                    >
-                        LOGO
-                    </Typography>
                     <Box sx={{flexGrow: 1, display: {xs: 'none', md: 'flex'}}}/>
-
-                    {isLoggedIn && <Box sx={{flexGrow: 0}}>
-                        <Tooltip title="Open settings">
-                            <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
-                                <Avatar alt={'avatar'} src="/static/images/avatar/2.jpg"/>
-                            </IconButton>
-                        </Tooltip>
-                        <Menu
-                            sx={{mt: '45px'}}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            {location.pathname !== '/profile' && <MenuItem onClick={handleCloseUserMenu}>
-                                <Typography textAlign="center"
-                                            onClick={profileHandler}
-                                            className={styles.menu}
+                    {isLoggedIn &&
+                        <>
+                            <div className={styles.userName}>{userName}</div>
+                            <Box sx={{flexGrow: 0}}>
+                                <Tooltip title="Open settings">
+                                    <IconButton onClick={handleOpenUserMenu} sx={{p: 0}}>
+                                        <Avatar alt={'avatar'}
+                                                src={userAvatar || defaultUser}
+                                        />
+                                    </IconButton>
+                                </Tooltip>
+                                <Menu
+                                    sx={{mt: '45px'}}
+                                    id="menu-appbar"
+                                    anchorEl={anchorElUser}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={Boolean(anchorElUser)}
+                                    onClose={handleCloseUserMenu}
                                 >
-                                    <img src={profile}
-                                         alt={'profile'}
-                                         className={styles.icon}
-                                    />
-                                    <span>Profile</span></Typography>
-                            </MenuItem>}
-                            <MenuItem onClick={handleCloseUserMenu}>
-                                <Typography textAlign="center"
-                                            onClick={logoutHandler}
-                                            className={styles.menu}
-                                >
-                                    <img src={logout}
-                                         alt={'logout'}
-                                         className={styles.icon}
-                                    />
-                                    <span>Logout</span></Typography>
-                            </MenuItem>
-                        </Menu>
-                    </Box>}
+                                    {location.pathname !== '/profile' &&
+                                        <MenuItem onClick={handleCloseUserMenu}>
+                                            <Typography textAlign="center"
+                                                        onClick={profileHandler}
+                                                        className={styles.menu}
+                                            >
+                                                <img src={profile}
+                                                     alt={'profile'}
+                                                     className={styles.icon}
+                                                />
+                                                <span>Profile</span></Typography>
+                                        </MenuItem>}
+                                    <MenuItem onClick={handleCloseUserMenu}>
+                                        <Typography textAlign="center"
+                                                    onClick={logoutHandler}
+                                                    className={styles.menu}
+                                        >
+                                            <img src={logout}
+                                                 alt={'logout'}
+                                                 className={styles.icon}
+                                            />
+                                            <span>Logout</span></Typography>
+                                    </MenuItem>
+                                </Menu>
+                            </Box>
+                        </>
+                    }
                 </Toolbar>
             </Container>
         </AppBar>
